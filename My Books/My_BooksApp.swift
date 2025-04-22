96dc573837492b335068787065039cf0455bbff0
Jonasodiq <jonasniyazson@hotmail.com>
2025-04-22 13:59:22 +0200

    Initial Commit

---
+++ My Books/My_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_BooksApp.swift
+//  My Books
+//
+//  Created by Jonas Niyazson on 2025-04-22.
+//
+
+import SwiftUI
+
+@main
+struct My_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
